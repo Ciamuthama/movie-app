5e3fd86ca08d0b794153c541c2d210579e72813f
ciamuthama <muthamapeter79@gmail.com>
2024-02-01 11:43:48 +0300

    Add: vercel speed insight

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 	import './styles.css';
-	
+	import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit"
+
+
 </script>
 
 <main class="mx-20 max-sm:mx-auto">
 	<slot />
+	{injectSpeedInsights()}
 </main>
