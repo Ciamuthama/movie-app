54b5391ffb10f732406305d9456a389513a43c2b
peter muthama <muthamapeter79@gmail.com>
2024-04-08 16:04:38 +0300

    minor fix

--- src/lib/cards/SimilarCard.svelte
+++ src/lib/cards/SimilarCard.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
     export let movie:any
+
+    const getYear = (date: any) => {
+		const d = new Date(date);
+		return d.getFullYear();
+	};
 </script>
 
 <div class="relative w-[25rem] max-sm:w-72">
@@ -11,5 +16,10 @@
     />
     <div
         class="absolute bottom-0  rounded-b bg-gradient-to-t from-black/80 rounded mt-3 object-fit h-56 max-sm:h-48 w-[25.1rem] max-sm:w-72"
-    ></div>
+    >
+<div class="absolute bottom-0 pl-4">
+    <p class="text-white font-bold">{movie.title}</p>
+    <p class="text-white">{getYear(movie.release_date)}</p>
+</div>
+</div>
 </div>
