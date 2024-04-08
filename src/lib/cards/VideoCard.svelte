b88bcb76ce10639eeee81c437f6dc430e4532e24
Peter Muthama <114055846+Ciamuthama@users.noreply.github.com>
2024-04-08 14:20:27 +0300

    Update VideoCard.svelte add watch movie

--- src/lib/cards/VideoCard.svelte
+++ src/lib/cards/VideoCard.svelte
@@ -31,8 +31,8 @@
 					<iframe
 						width="560"
 						height="315"
-						src="https://www.youtube.com/embed/{movie.key}"
-						title="YouTube video player"
+						src="https://vidsrc.xyz/embed/movie/{movie.id}"
+						title={movie.title}
 						frameborder="0"
 						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
 						allowfullscreen
